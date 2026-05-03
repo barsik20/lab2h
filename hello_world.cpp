eed3f87bb05c9fdb11287fa259650e5ee9b2494d
barsik20 <bars.070620077777@gmail.com>
2026-05-03 19:28:59 +0300

    Не используем using namespace

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
 #include <string>
 
-int main(){
-    // Переменная для хранения
+int main() {
     std::string name;
-    // Ввод имени
     std::cout << "Enter your name: ";
     std::cin >> name;
     std::cout << "Hello world from @" << name << std::endl;
