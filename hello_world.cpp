346dcc5fe0c9675579b1335f97b191f196c9c95c
barsik20 <bars.070620077777@gmail.com>
2026-05-03 19:35:55 +0300

    Добавлены комментарии в код

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <string>
 
+// Main function
 int main() {
+    // user's name
     std::string name;
     std::cout << "Enter your name: ";
+    // entering your name
     std::cin >> name;
     std::cout << "Hello world from @" << name << std::endl;
     return 0;
