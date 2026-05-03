18ac782d48352aac5b70087f348a5e26eeb7edab
barsik20 <bars.070620077777@gmail.com>
2026-05-03 20:13:38 +0300

    Изменены комментарии в main

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
 #include <string>
-
-// Главная функция
 int main(){
-   // имя пользователя
+    // Переменная для хранения
     std::string name;
-    //Ввод имени
+    // Ввод имени
     std::cout << "Enter your name: ";
     std::cin >> name;
     std::cout << "Hello world from @" << name << std::endl;
