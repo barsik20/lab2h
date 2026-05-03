b33ad566da8d9c9b51cdc74b72d6ade72c67e633
barsik20 <bars.070620077777@gmail.com>
2026-05-03 20:00:04 +0300

    Поменяли язык

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <string>
-using namespace std;
-int main() {
-    string name;
-    cout << "Enter your name: ";
-    cin >> name;
-    cout << "Hello world from @" << name << endl;
+
+// Главная функция
+int main(){
+   // имя пользователя
+    std::string name;
+    //Ввод имени
+    std::cout << "Enter your name: ";
+    std::cin >> name;
+    std::cout << "Hello world from @" << name << std::endl;
     return 0;
 }
